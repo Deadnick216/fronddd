9be6d37901169c80528ee6b5ea5e955de61dad4f
Deadnick216 <89610678+Deadnick216@users.noreply.github.com>
2024-04-15 22:31:28 -0400

    Add files via upload

---
+++ Router.tsx
@@ -0,0 +1,45 @@
+import { NavigationContainer } from '@react-navigation/native';
+import Loading from './src/screens/loading';
+import Login from './src/screens/login';
+import Register from './src/screens/register';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/home';
+import RecuperarContra from './src/screens/recuperarContra'; // Cambio aquí
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export type RootStackParamList = {
+  Loading: Record<string, string> | undefined;
+  Home: Record<string, string> | undefined;
+  Register: Record<string, string> | undefined;
+  Login: Record<string, string> | undefined;
+};
+
+const RouterProvider = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Loading"
+          component={Loading}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Register"
+          component={Register}
+        />
+        <Stack.Screen name="Home" component={Home} />
+        {/*<Stack.Screen name="recuperarContra" component={RecuperarContra} /> {/* Cambio aquí */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RouterProvider;
+
